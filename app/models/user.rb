698a7443246cde740754cc8dd390214451c77328
RailonA <railonacosta@gmail.com>
2021-02-19 15:38:07 -0500

    Add validation and format for email and password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
-  has_secure_password
+    has_secure_password
+    validates_uniqueness_of :user_name, :email
+
+    validates :user_name, presence: true
+    validates :email, presence: true, format: {with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/, message: "must be a valid e-mail address"}
+    validates :password_digest, presence: true, format: {with: /\A[(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}]+/, message:'Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character:' }
+
 end
