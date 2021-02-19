8b8028f93d89760c44de3407fc8c687d382deaa3
RailonA <railonacosta@gmail.com>
2021-02-19 16:25:38 -0500

    Add Post DB

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    has_many :post
     has_secure_password
     validates_uniqueness_of :user_name, :email
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_19_195158) do
+ActiveRecord::Schema.define(version: 2021_02_19_210650) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "url_link"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "user_name", null: false
@@ -20,4 +28,5 @@ ActiveRecord::Schema.define(version: 2021_02_19_195158) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "posts", "users"
 end
