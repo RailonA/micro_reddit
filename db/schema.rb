eefe5cb41e1c7c79bf2c7e77d70e5118a1cafbd1
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-02-22 21:31:25 +0100

    Update the association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_22_150359) do
+ActiveRecord::Schema.define(version: 2021_02_22_201413) do
 
   create_table "comments", force: :cascade do |t|
     t.string "comment_body"
@@ -29,9 +29,9 @@ ActiveRecord::Schema.define(version: 2021_02_22_150359) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "user_name", null: false
-    t.string "email", null: false
-    t.string "password_digest", null: false
+    t.string "user_name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
