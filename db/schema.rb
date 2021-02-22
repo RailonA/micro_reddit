3601bd7646f4a5f3745b9a948b74e5d088afd13c
RailonA <railonacosta@gmail.com>
2021-02-22 14:38:53 -0500

    Add last table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_19_210650) do
+ActiveRecord::Schema.define(version: 2021_02_22_150359) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "comment_body"
+    t.integer "post_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "url_link"
@@ -28,5 +36,6 @@ ActiveRecord::Schema.define(version: 2021_02_19_210650) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "comments", "posts"
   add_foreign_key "posts", "users"
 end
